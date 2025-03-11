571481d320f7cefed8b9837b63a5dd471cca2100
dinushatharindu <tvdtharindu@gmail.com>
2025-03-11 20:28:20 +0530

    lag

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-rg"
-  location = var.location
+  location = "us-west"
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "prefix" {
-
+    default = "us-west"
 }
 
 variable "location" {
